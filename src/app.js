763f696b14746fdf5b7a9c2325a4a71672bb7346
kuRajj <epideimya@gmail.com>
2022-10-24 16:15:51 +0300

    add classname to changeColorBtn

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import { getRandomColor } from './utils'
 
 export default function initApp() {
     const changeColorBtn = document.createElement('button');
+    changeColorBtn.className = 'button'
     changeColorBtn.textContent = 'Изменить цвет страницы';
     document.body.append(changeColorBtn);
     changeColorBtn.addEventListener('click', () => {
